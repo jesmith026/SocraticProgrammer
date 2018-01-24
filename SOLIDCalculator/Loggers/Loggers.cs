2aea7f61c7d016f5988999b8fcf70f4f63eaa96c
James.Smith <James.Smith@IHSMarkit.com>
2018-01-23 18:41:26 -0600

    SOLID-SRP

---
+++ SOLIDCalculator/Loggers/Loggers.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SOLIDCalculator.Loggers
+{
+    public interface ILogger
+    {
+        void Log(string message);
+    }
+
+    public class ConsoleLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    public class FileLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            using (var file = new System.IO.StreamWriter(@"C:\Temp\AreaLog.txt"))
+            {
+                file.WriteLine(message);
+            }
+        }
+    }
+
+    public class MessageBoxLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            MessageBox.Show(message);
+        }
+    }
+}
